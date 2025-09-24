d07c0ebc2e7c1bdc1681991a303a2afb1825ee0a
Velaguez Szpiech <148214947+VelaguezSzpiech@users.noreply.github.com>
2025-09-24 23:10:04 +0200

    Fix strict color parsing and map validation

--- src/parsing/parse_colors.c
+++ src/parsing/parse_colors.c
@@ -6,102 +6,99 @@
 /*   By: ldurmish < ldurmish@student.42wolfsburg.d  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:06:45 by ldurmish          #+#    #+#             */
-/*   Updated: 2025/09/24 22:54:17 by ldurmish         ###   ########.fr       */
+/*   Updated: 2025/09/30 10:24:45 by ldurmish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/parsing.h"
 
-void	skip_ws(const char **str)
+static void     skip_leading_ws(const char **str)
 {
-	const char		*p;
+        const char      *p;
 
-	p = *str;
-	while (*p == ' ' || *p == '\t')
-		p++;
-	*str = p;
+        p = *str;
+        while (*p == ' ' || *p == '\t')
+                p++;
+        *str = p;
 }
 
-int	read_num_0_255(const char **str, int *color)
+static int      read_component(const char **str, int *color)
 {
-	const char		*p;
-	int				val;
-	int				has_digit;
+        const char      *p;
+        int             val;
 
-	p = *str;
-	skip_ws(&p);
-	val = 0;
-	has_digit = 0;
-	while (*p && ft_isdigit((unsigned char)*p))
-	{
-		has_digit = 1;
-		val = val * 10 + (*p - '0');
-		if (val < 0 || val > 255)
-			return (perr(P_EFMT, "color out of range"), 1);
-		p++;
-	}
-	if (!has_digit)
-		return (perr(P_EFMT, "expected color number"));
-	skip_ws(&p);
-	*color = val;
-	*str = p;
-	return (0);
+        if (!str || !*str || !color)
+                return (perr(P_EARG, "read_component bad args"));
+        p = *str;
+        if (!ft_isdigit((unsigned char)*p))
+                return (perr(P_EFMT, "expected color number"));
+        val = 0;
+        while (*p && ft_isdigit((unsigned char)*p))
+        {
+                val = val * 10 + (*p - '0');
+                if (val < 0 || val > 255)
+                        return (perr(P_EFMT, "color out of range"));
+                p++;
+        }
+        *color = val;
+        *str = p;
+        return (0);
 }
 
-static int	expect_comma(const char **str)
+static int      expect_comma(const char **str)
 {
-	const char	*s;
+        const char      *p;
 
-	s = *str;
-	skip_ws(&s);
-	if (*s != ',')
-		return (perr(P_EFMT, "expected comma"));
-	s++;
-	skip_ws(&s);
-	*str = s;
-	return (0);
+        if (!str || !*str)
+                return (perr(P_EARG, "expect_comma bad args"));
+        p = *str;
+        if (*p != ',')
+                return (perr(P_EFMT, "expected comma"));
+        p++;
+        if (*p == ' ' || *p == '\t')
+                return (perr(P_EFMT, "spaces around comma not allowed"));
+        *str = p;
+        return (0);
 }
 
-int	parse_rgb_triplet(const char *s, int *r, int *g, int *b)
+int     parse_rgb_triplet(const char *s, int *r, int *g, int *b)
 {
-	const char		*p;
+        const char      *p;
 
-	if (!s || !r || !g || !b)
-		return (perr(P_EARG, "parse_rgb_triplet bad args"));
-	p = s;
-	if (read_num_0_255(&p, r) != 0)
-		return (1);
-	skip_ws(&p);
-	if (expect_comma(&p) != 0)
-		return (1);
-	skip_ws(&p);
-	if (read_num_0_255(&p, g) != 0)
-		return (1);
-	skip_ws(&p);
-	if (expect_comma(&p) != 0)
-		return (1);
-	skip_ws(&p);
-	if (read_num_0_255(&p, b) != 0)
-		return (1);
-	skip_ws(&p);
-	if (*p != '\0' && *p != '\n' && *p != '\r')
-		return (perr(P_EFMT, "trailing characters after color"));
-	return (0);
+        if (!s || !r || !g || !b)
+                return (perr(P_EARG, "parse_rgb_triplet bad args"));
+        p = s;
+        skip_leading_ws(&p);
+        if (read_component(&p, r) != 0)
+                return (1);
+        if (expect_comma(&p) != 0)
+                return (1);
+        if (read_component(&p, g) != 0)
+                return (1);
+        if (expect_comma(&p) != 0)
+                return (1);
+        if (read_component(&p, b) != 0)
+                return (1);
+        while (*p == ' ' || *p == '\t')
+                p++;
+        if (*p != '\0' && *p != '\n' && *p != '\r')
+                return (perr(P_EFMT, "trailing characters after color"));
+        return (0);
 }
 
-int	parse_color(const char *s, t_color *dst)
+int     parse_color(const char *s, t_color *dst)
 {
-	int		r;
-	int		g;
-	int		b;
+        int             r;
+        int             g;
+        int             b;
 
-	if (!dst)
-		return (perr(P_EARG, "parse_color bad arg"));
-	if (parse_rgb_triplet(s, &r, &g, &b) != 0)
-		return (1);
-	dst->r = r;
-	dst->g = g;
-	dst->b = b;
-	dst->value = ((r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF));
-	return (0);
+        if (!dst)
+                return (perr(P_EARG, "parse_color bad arg"));
+        if (parse_rgb_triplet(s, &r, &g, &b) != 0)
+                return (1);
+        dst->r = r;
+        dst->g = g;
+        dst->b = b;
+        dst->value = ((r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF));
+        return (0);
 }
