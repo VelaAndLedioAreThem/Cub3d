614916fbe9fafcbf7eb5e4782d801bec662f7f2e
Velaguez <velaguez1@gmail.com>
2025-09-26 12:57:56 +0200

    fixed texture morphing issue

--- src/execution/render_walls.c
+++ src/execution/render_walls.c
@@ -34,45 +34,52 @@ static void	wall_bounds(int height, int *top, int *bot)
 		*bot = SCREEN_HEIGHT - 1;
 }
 
-static void	draw_column_params(t_game *game, int x, t_texture *tex, int *p)
+static void	draw_column_params(t_game *game, int x, t_texture *tex,
+                        int top, int bot, int tex_x, int line_height)
 {
-	double	step;
-	double	tex_pos;
-	int		y;
-	int		tex_y;
+    double  step;
+    double  tex_pos;
+    int     y;
+    int     tex_y;
 
-	step = (double)tex->height / (double)p[3];
-	tex_pos = 0.0;
-	y = p[0];
-	while (y < p[0] + p[3])
-	{
-		tex_y = (int)tex_pos;
-		if (tex_y < 0)
-			tex_y = 0;
-		if (tex_y >= tex->height)
-			tex_y = tex->height - 1;
-		put_pixel(game, x, y, get_texture_color(tex, p[2], tex_y));
-		tex_pos += step;
-		y++;
-	}
+    /*
+     * Use the unclipped wall height for vertical stepping to avoid texture
+     * distortion when the wall is taller than the screen. Start the texture
+     * at the correct offset if the top got clipped.
+     */
+    step = (double)tex->height / (double)line_height;
+    tex_pos = ((double)top - ((double)SCREEN_HEIGHT / 2.0 - (double)line_height / 2.0)) * step;
+    y = top;
+    while (y <= bot)
+    {
+        tex_y = (int)tex_pos;
+        if (tex_y < 0)
+            tex_y = 0;
+        if (tex_y >= tex->height)
+            tex_y = tex->height - 1;
+        put_pixel(game, x, y, get_texture_color(tex, tex_x, tex_y));
+        tex_pos += step;
+        y++;
+    }
 }
 
 void	render_wall_strip(t_game *game, int strip_id)
 {
-	int			p[4];
-	int			height;
-	t_texture	*tex;
-	t_ray		*ray;
+    int         top;
+    int         bot;
+    int         height;
+    int         tex_x;
+    t_texture   *tex;
+    t_ray       *ray;
 
-	ray = &game->rays[strip_id];
-	height = corrected_height(game, ray);
-	wall_bounds(height, &p[0], &p[1]);
-	tex = select_wall_texture(game, ray);
-	if (!tex || !tex->addr)
-		return ;
-	p[2] = calculate_texture_x(ray, tex);
-	p[3] = p[1] - p[0] + 1;
-	draw_column_params(game, strip_id, tex, p);
+    ray = &game->rays[strip_id];
+    height = corrected_height(game, ray);
+    wall_bounds(height, &top, &bot);
+    tex = select_wall_texture(game, ray);
+    if (!tex || !tex->addr)
+        return ;
+    tex_x = calculate_texture_x(ray, tex);
+    draw_column_params(game, strip_id, tex, top, bot, tex_x, height);
 }
 
 void	render_walls(t_game *game)
